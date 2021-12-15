96b6e048331d19683a4159eab01e299b2729cd92
AlexanderKohr <94537064+AlexanderKohr@users.noreply.github.com>
2021-12-15 17:33:06 +0100

    created pokemonList variable, assigned an array and added pokemons as objects to it

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,38 @@
-alert('Hello world');
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
-
+let pokemonList = [
+    {
+        name: 'Venomoth',
+        type: ['bug', 'poison'],
+        height: 1.5,
+        pokedexNumber: '#49'
+    },
+    {
+        name: 'Mr. Mime',
+        type: ['psychic', 'fairy'],
+        height: 1.3,
+        pokedexNumber: '#122'
+    },
+    {
+        name: 'Tyranitar',
+        type: ['dark', 'rock'],
+        height: 2,
+        pokedexNumber: '#248'
+    },
+    {
+        name: 'Scizor',
+        type: ['steel', 'bug'],
+        height: 1.8,
+        pokedexNumber: '#212'
+    },
+    {
+        name: 'Jigglypuff',
+        type: ['fairy', 'normal'],
+        height: 0.5,
+        pokedexNumber: '#39'
+    },
+    {
+        name: 'Ditto',
+        type: 'normal',
+        height: 0.3,
+        pokedexNumber: '#132'
+    }
+];
