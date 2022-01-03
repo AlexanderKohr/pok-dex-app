f2520f716efbb61550a253fd6f25313a156a5996
AlexanderKohr <94537064+AlexanderKohr@users.noreply.github.com>
2022-01-03 19:18:20 +0100

    Update scripts.js
    Wrapped the pokemonList array in IIFE and gave it two public functions.
    Changed the for loop to forEach to iterate over the pokemon in pokemonList array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,68 @@
-let pokemonList = [
-    {
-        name: 'Venomoth',
-        type: ['bug', 'poison'],
-        height: 1.5,
-        pokedexNumber: '#49'
-    },
-    {
-        name: 'Mr. Mime',
-        type: ['psychic', 'fairy'],
-        height: 1.3,
-        pokedexNumber: '#122'
-    },
-    {
-        name: 'Tyranitar',
-        type: ['dark', 'rock'],
-        height: 2,
-        pokedexNumber: '#248'
-    },
-    {
-        name: 'Scizor',
-        type: ['steel', 'bug'],
-        height: 1.8,
-        pokedexNumber: '#212'
-    },
-    {
-        name: 'Jigglypuff',
-        type: ['fairy', 'normal'],
-        height: 0.5,
-        pokedexNumber: '#39'
-    },
-    {
-        name: 'Ditto',
-        type: 'normal',
-        height: 0.3,
-        pokedexNumber: '#132'
-    }
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Venomoth',
+            type: ['bug', 'poison'],
+            height: 1.5,
+            pokedexNumber: '#49'
+        },
+        {
+            name: 'Mr. Mime',
+            type: ['psychic', 'fairy'],
+            height: 1.3,
+            pokedexNumber: '#122'
+        },
+        {
+            name: 'Tyranitar',
+            type: ['dark', 'rock'],
+            height: 2,
+            pokedexNumber: '#248'
+        },
+        {
+            name: 'Scizor',
+            type: ['steel', 'bug'],
+            height: 1.8,
+            pokedexNumber: '#212'
+        },
+        {
+            name: 'Jigglypuff',
+            type: ['fairy', 'normal'],
+            height: 0.5,
+            pokedexNumber: '#39'
+        },
+        {
+            name: 'Ditto',
+            type: 'normal',
+            height: 0.3,
+            pokedexNumber: '#132'
+        }
+    ];
 
-// for loop that displays all pokemons of pokemonList with name an height
-for (let i=0; i < pokemonList.length; i++){
-    document.write(pokemonList[i].name + 
-    ' (' + 'height: ' + 
-    pokemonList[i].height + ')'+ ("<br>"));
-// conditional for pokemon with a heigth more than 1.9
-    if (pokemonList[i].height > 1.9){
-        document.write(' -' + 'Wow, that\'s big! ');
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-  }
 
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+
+})();
+
+
+pokemonRepository.add({
+    name: 'Blitzle',
+    type: 'electric',
+    height: 0.8,
+    pokedexNumber: '#522'
+});
+
+
+// forEach loop to iterate over the pokemon in pokemonList
+pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write('<p>' + pokemon.name + ' (' + 'height: ' + pokemon.height + ') ' + pokemon.pokedexNumber + '</p>')
+});
